bc52cdb19307ec9e7f035c2582d834dc5967825e
shanthibabu <73182461+shanthi1999@users.noreply.github.com>
2021-09-07 10:14:15 +0530

    Add files via upload

---
+++ client/src/AnotherMethod.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { render } from "react-dom";
+import useRecorder from "./useRecoder";
+
+const AnotherMethod = props => {
+    let [audioURL, isRecording, startRecording, stopRecording] = useRecorder();
+    return (
+      <div className="App">
+        <audio src={audioURL} controls />
+        <button onClick={startRecording} disabled={isRecording}>
+          start recording
+        </button>
+        <button onClick={stopRecording} disabled={!isRecording}>
+          stop recording
+        </button>
+  
+        <p>
+          <em>
+            (On Codesandbox pop out the preview into a window to get a user media
+            request.)
+          </em>
+        </p>
+      </div>
+    );
+  
+};
+
+
+export default AnotherMethod;
